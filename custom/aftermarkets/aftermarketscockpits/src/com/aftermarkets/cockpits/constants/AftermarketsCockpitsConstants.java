58189c5c7d44d1e6b4d084f6d528f22cd7845524
US\armuruganandham <armuruganandham@deloitte.com>
2017-12-18 15:22:09 -0800

    Initial code base setup

---
+++ custom/aftermarkets/aftermarketscockpits/src/com/aftermarkets/cockpits/constants/AftermarketsCockpitsConstants.java
@@ -0,0 +1,26 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2017 SAP SE or an SAP affiliate company.  All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package com.aftermarkets.cockpits.constants;
+
+/**
+ * Global class for all AftermarketsCockpits constants. You can add global constants for your extension into this class.
+ */
+public final class AftermarketsCockpitsConstants extends GeneratedAftermarketsCockpitsConstants
+{
+	public static final String EXTENSIONNAME = "aftermarketscockpits";
+
+	public static final String JASPER_REPORTS_MEDIA_FOLDER = "jasperreports";
+
+	private AftermarketsCockpitsConstants()
+	{
+		// empty
+	}
+}
